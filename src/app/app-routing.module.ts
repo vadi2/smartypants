8cbf71621222ffb20b7b6e967a21964a2a3be096
Vadim Peretokin <vperetokin@gmail.com>
2019-08-28 16:14:56 +0200

    Add page-not-found component and fix /launch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LaunchComponent } from './launch/launch.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'launch', component: LaunchComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
+  // declarations: [LaunchComponent],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
