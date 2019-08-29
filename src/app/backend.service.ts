dc0d84b63b05c574b576575b8658b5db78f1ea30
Vadim Peretokin <vperetokin@gmail.com>
2019-08-29 13:09:49 +0200

    Start on backend class

--- src/app/backend.service.ts
+++ src/app/backend.service.ts
@@ -1,16 +1,48 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackendService {
+  iss: string;
+  launch: string;
+  readonly wellKnownLocation = '/.well-known/smart-configuration.json';
+  readonly metadataLocation = "/metadata";
+
+  constructor(private http: HttpClient) { }
+
+  setLaunchParameters(launch: string, iss: string) {
+    this.launch = launch;
+    this.iss = iss;
+  }
+
   locateEndpoints() {
 
   }
 
   fetchWellKnownConfiguration() {
+    this.http.get(this.iss + this.wellKnownLocation).subscribe((res: Response) => {
+      console.log(res);
+    }, error => {
+      if (error.status !== 404) {
+        console.log(error);
+      }
 
+      this.fetchOAuthFromMetadata();
+    });
   }
 
-  constructor() { }
+  fetchOAuthFromMetadata() {
+    this.http.get<[]>(this.iss + this.metadataLocation).subscribe((res: {}) => {
+      this.extractOAuthFromMetadata(res);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  extractOAuthFromMetadata(metadata: {}) {
+    console.log('extracting');
+    console.log(metadata.rest);
+  }
 }
