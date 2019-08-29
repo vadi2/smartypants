0f60d042b59932dc8fae94304b94c50dcbbbcfde
Vadim Peretokin <vperetokin@gmail.com>
2019-08-29 13:10:00 +0200

    Wire in and use backend service

--- src/app/launch/launch.component.ts
+++ src/app/launch/launch.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { BackendService } from '../backend.service';
 
 @Component({
   selector: 'app-launch',
@@ -9,7 +10,10 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./launch.component.css']
 })
 export class LaunchComponent implements OnInit {
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private backend: BackendService
+  ) { }
 
   iss: string;
   launch: string;
@@ -20,5 +24,8 @@ export class LaunchComponent implements OnInit {
 
     this.route.queryParamMap
       .subscribe(p => this.launch = p.get('launch'));
+
+    this.backend.setLaunchParameters(this.launch, this.iss);
+    this.backend.fetchWellKnownConfiguration();
   }
 }
