f3f7466a540790c8893053ef6ed140d291b04521
Vadim Peretokin <vperetokin@gmail.com>
2019-08-30 10:44:04 +0200

    Start wiring in OAuth redirect receiver

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LaunchComponent } from './launch/launch.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { OauthRedirectComponent } from './oauth-redirect/oauth-redirect.component';
 
 const routes: Routes = [
   { path: 'launch', component: LaunchComponent },
+  { path: 'oauth-redirect', component: OauthRedirectComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
 
